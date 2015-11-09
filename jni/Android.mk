c8b00c7e773066824a07a81a1017ef585ea7b805
aliaspider <aliaspider@gmail.com>
2015-11-09 10:01:33 +0100

    (android) enable ARM_ASM only for TARGET_ARCH=arm

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,21 +2,20 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ARM_ASM = 1
-
 LOCAL_MODULE    := retro
 
 ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
+ARM_ASM         = 1
+LOCAL_CFLAGS   += -DANDROID_ARM
 LOCAL_ARM_MODE := arm
 endif
 
 ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
+LOCAL_CFLAGS   +=  -DANDROID_X86
 endif
 
 ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
+LOCAL_CFLAGS   += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
 endif
 
 ifeq ($(NDK_DEBUG),1)
