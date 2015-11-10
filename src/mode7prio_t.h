deda34b44a796ad6fbac100355eccb522b2e5d9f
twinaphex <libretro@gmail.com>
2015-11-10 04:10:09 +0100

    Remove unused variables

--- src/mode7prio_t.h
+++ src/mode7prio_t.h
@@ -39,7 +39,6 @@ static void DrawBGMode7Background16R3(uint8* Screen, int bg)
    int DD;
    uint32 Line;
    uint32 clip;
-   uint8 b;
    uint8* Depth;
    unsigned int fixedColour = GFX.FixedColour;
    uint32 depth = Mode7Depths[0] | (Mode7Depths[1] << 8);
@@ -238,7 +237,6 @@ static void DrawBGMode7Background16R1R2(uint8* Screen, int bg)
    int DD;
    uint32 Line;
    uint32 clip;
-   uint8 b;
    uint32 AndByY;
    uint32 AndByX = 0xffffffff;
    if (Settings.Dezaemon && PPU.Mode7Repeat == 2) AndByX = 0x7ff;
