2dcd8f8d79ea138883e36c4d1650e928afdf3266
twinaphex <libretro@gmail.com>
2016-08-08 05:15:05 +0200

    Cleanups

--- src/display.h
+++ src/display.h
@@ -46,12 +46,10 @@ START_EXTERN_C
 void S9xSetPalette();
 void S9xTextMode();
 void S9xGraphicsMode();
-void S9xExtraUsage();
 uint32 S9xReadJoypad(int which1_0_to_4);
 bool8_32 S9xReadMousePosition(int which1_0_to_1, int* x, int* y, uint32* buttons);
 bool8_32 S9xReadSuperScopePosition(int* x, int* y, uint32* buttons);
 
-void S9xUsage();
 void S9xInitDisplay(int argc, char** argv);
 void S9xDeinitDisplay();
 void S9xInitInputDevices();
@@ -60,11 +58,7 @@ void S9xProcessEvents(bool8_32 block);
 void S9xPutImage(int width, int height);
 void S9xToggleSoundChannel(int channel);
 void S9xSetInfoString(const char* string);
-int S9xMinCommandLineArgs();
 void S9xNextController();
-bool8_32 S9xLoadROMImage(const char* string);
-const char* S9xSelectFilename(const char* def, const char* dir,
-                              const char* ext, const char* title);
 
 const char* S9xBasename(const char* filename);
 
